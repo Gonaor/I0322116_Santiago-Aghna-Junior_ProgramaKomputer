3ba1e3580fb519a5d37b7fec218579c5b75544d2
Aghna SJ <santiagoaghnajunior2003@gmail.com>
2023-03-11 22:23:28 +0700

    Exercise_01.py

---
+++ Belajar_Coding/Tutor Posi.py
@@ -0,0 +1,20 @@
+#string
+Nama_Depan = "Vorsteiner"
+Nama_Belakang = "De la Roza"
+#integer
+Angka = 10
+#desimal
+Desimal = 3.5
+#boolean
+Boolean = True
+#list
+Kelas_D = ["Vorsteiner", "Kelas D", "I0322116"]
+#dictionary
+Dictionary = {"Nama": "Vorsteiner", "Kelas": "Kelas D"}
+
+print ("Nama saya adalah Vorsteiner De la Roza")
+print (f"Nama panggilan saya adalah {Nama_Depan}")
+print ("Namun saya mempunyai nama alias, yaitu Beltrame")
+print (f"{Nama_Belakang} adalah Marga dari orang tua saya")
+print (f"Saat ini saya berumur {Angka} tahun")
+print (f"{Desimal} adalah IPK saat aku sarjana")
